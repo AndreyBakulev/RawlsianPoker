17d090b206fe75121718981ccf1e3b3af8b3f863
AndreyBakulev <gengubae@gmail.com>
2024-04-16 12:50:24 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+/*
+TODO:
+add a player class
+each player has an array of Traits
+each trait affects the total money
+make a card class that has a shuffle method and yea
+
+
+
+ */
